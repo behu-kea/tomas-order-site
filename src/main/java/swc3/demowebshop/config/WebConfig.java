1ba1e1457ad86759719c2719457d8c67338bc832
Benjamin Hughes <behu@kea.dk>
2021-10-25 14:50:48 +0200

    asd

--- src/main/java/swc3/demowebshop/config/WebConfig.java
+++ src/main/java/swc3/demowebshop/config/WebConfig.java
@@ -15,10 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:5500",
                         "http://127.0.0.1:5500",
-                        "https://behu-kea.github.io/fullstact-frontend",
+                        "https://behu-kea.github.io/fullstact-frontend/*",
                         "https://behu-kea.github.io",
-                        "https://swc3-react-frontend.herokuapp.com",
-                        "https://behu-kea.github.io/fullstact-frontend/#/fullstact-frontend")
+                        "https://swc3-react-frontend.herokuapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true)
                 .maxAge(3600); //1 hour
